fb39f4a745a98a854c57c1a66b9d6e984777508f
Anderson Mello <andersonmello722@gmail.com>
2022-07-01 15:46:28 -0300

    Requisito 1

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">A</div>
+    );
+  }
+}
+
+export default NotFound;
