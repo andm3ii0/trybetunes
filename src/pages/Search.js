c7521661774d709c4af537180f3a9d45d380df93
Anderson Mello <andersonmello722@gmail.com>
2022-07-01 18:38:20 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,46 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      buttonDisabled: true,
+    };
+  }
+
+  onHandleChange = ({ target }) => {
+    const { value } = target;
+    const caracteresButonEnabled = 2;
+    this.setState({ search: value,
+      buttonDisabled: value.length < caracteresButonEnabled,
+    });
+  }
+
   render() {
+    const { search, buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search">
+            <input
+              name="search"
+              value={ search }
+              id="serch"
+              data-testid="search-artist-input"
+              onChange={ this.onHandleChange }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ buttonDisabled }
+            >
+              Pesquisar
+
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
